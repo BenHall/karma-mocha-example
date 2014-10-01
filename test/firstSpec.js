2f3cca1dada9f5710141a76dea7526e52084f72d
Ben Hall <ben@benhall.me.uk>
2014-10-01 17:12:52 +0100

    Example of running mocha and karma

---
+++ test/firstSpec.js
@@ -0,0 +1,8 @@
+describe('first spec', function() {
+  it('should pass', function() {
+    expect(true).to.be.true; 
+  });
+  it('should false', function() {
+    expect(true).to.be.false; 
+  });
+});
